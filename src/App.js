99672c17d0ad0669e5fabf18c875aa151c2f33b4
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 23:53:52 -0300

    Update App.js
    inline style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-const Li = ({children}) => {
+const estilo2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,0.5)'
+}
+
+const estilo = (bg = '#333') =>({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+})
+
+const Li = ({ children }) => {
   return (
-    <li className='clase-li'>{children}</li>
+    <li style={{...estilo2, ...estilo()}} className='clase-li'>{children}</li>
   )
 
 
@@ -12,7 +23,7 @@ const App = () => {
   const valor = 'asdsafgg'
   return (
 
-    <ul className="clase-css">Chanchito {valor}
+    <ul style={estilo('#750')} className="clase-css">
       <Li estado="feliz">Valor de li</Li>
     </ul>
   );
