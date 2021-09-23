3e523c3cf544b845f34dc895ec44a5f2d62a0baa
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-23 00:23:20 -0300

    Update App.js
    Listas y keys

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import Button from "./Button"
 
+const arr = [
+    'chanchito feliz',
+    'chanchito triste',
+    'chanchito emocionado',
+]
+
 const App = () => {
 
-    const miVariable = true;
+    const miVariable = false;
 
-    if(miVariable){
+    if (miVariable) {
         return <p>Mi variable dio True!</p>
     }
 
     return (
         <div>
-            <h1 onClick={(e) => console.log('click', e) }>
+            <h1 onClick={(e) => console.log('click', e)}>
                 Hola mundo
             </h1>
+            {arr.map((el) => <p key={el}>{el}</p>)}
             <Button onClick={() => console.log('clickeado')}>
                 Enviar
             </Button>
