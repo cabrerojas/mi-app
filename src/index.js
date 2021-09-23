b2ad9f613bb91f3528ca4ebe64dc70df4d510ef7
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 23:44:59 -0300

    componentes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 //const x = React.createElement('p',null, 'hola mundo')
 
-const Li = ({ children, estado, casa, edad }) => {
-  console.log(casa, edad)
-  return (
-    <li>{children} {estado}</li>
-  )
-}
+// const Li = ({ children, estado, casa, edad }) => {
+//   console.log(casa, edad)
+//   return (
+//     <li>{children} {estado}</li>
+//   )
+// }
 
 
-const X = () =>
-  <ul>
-    <Li
-      estado={'feliz'}
-      casa={false}
-      edad={21}
-    >
-      Chanchito
-    </Li>
-    <Li estado={'triste'} >Chanchito</Li>
-    <Li estado={'emocionado'} >Chanchito</Li>
+// const X = () =>
+//   <ul>
+//     <Li
+//       estado={'feliz'}
+//       casa={false}
+//       edad={21}
+//     >
+//       Chanchito
+//     </Li>
+//     <Li estado={'triste'} >Chanchito</Li>
+//     <Li estado={'emocionado'} >Chanchito</Li>
 
-  </ul>
+//   </ul>
 
-ReactDOM.render(
-  <X />, document.getElementById('root')
-) // React.createElement()
+// ReactDOM.render(
+//   <X />, document.getElementById('root')
+// ) // React.createElement()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
