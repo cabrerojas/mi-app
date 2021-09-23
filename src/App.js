77c84f5822b16af0d95782d5dc2d0a966555c292
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-23 00:16:56 -0300

    Update App.js
    renderizado condicional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Button from "./Button"
 
 const App = () => {
+
+    const miVariable = true;
+
+    if(miVariable){
+        return <p>Mi variable dio True!</p>
+    }
+
     return (
         <div>
-            <h1>
+            <h1 onClick={(e) => console.log('click', e) }>
                 Hola mundo
             </h1>
             <Button onClick={() => console.log('clickeado')}>
