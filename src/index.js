76b162f7b970156c3618870a98ef16e1d37dc43d
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 23:09:29 -0300

    Update index.js
    Render JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+//const x = React.createElement('p',null, 'hola mundo')
+
+const X = () => <p>Hola mundo!</p>
+
+  ReactDOM.render(
+    <X />, document.getElementById('root')
+  ) // React.createElement()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
