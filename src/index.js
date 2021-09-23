364cd4bec260a449e05a00457c1122c284c90658
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-09-22 23:18:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,11 +13,31 @@ import reportWebVitals from './reportWebVitals';
 
 //const x = React.createElement('p',null, 'hola mundo')
 
-const X = () => <p>Hola mundo!</p>
+const Li = ({ children, estado, casa, edad }) => {
+  console.log(casa, edad)
+  return (
+    <li>{children} {estado}</li>
+  )
+}
 
-  ReactDOM.render(
-    <X />, document.getElementById('root')
-  ) // React.createElement()
+
+const X = () =>
+  <ul>
+    <Li
+      estado={'feliz'}
+      casa={false}
+      edad={21}
+    >
+      Chanchito
+    </Li>
+    <Li estado={'triste'} >Chanchito</Li>
+    <Li estado={'emocionado'} >Chanchito</Li>
+
+  </ul>
+
+ReactDOM.render(
+  <X />, document.getElementById('root')
+) // React.createElement()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
